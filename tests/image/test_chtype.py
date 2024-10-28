226866af9684b02c1183b55a7534d0c60b264822
Gregory Vetkin <gregoryvetkin@gmail.com>
2024-10-28 14:43:49 +0300

    Added tests for one.image.chtype

--- tests/image/test_chtype.py
+++ tests/image/test_chtype.py
@@ -14,10 +14,12 @@ IMAGE_TYPES = ["OS", "CDROM", "DATABLOCK"]
 FILE_TYPES  = ["KERNEL", "RAMDISK", "CONTEXT"]
 
 ERROR_INCOMPATIBLE_TYPE = "Cannot change image type to an incompatible type for the current datastore"
+ERROR_GETTING_IMAGE     = "Error getting image"
+
 
 
 @pytest.fixture
-def prepare_image():
+def prepare_image_for_image_ds():
     image_name  = "api_test_image"
     image_id    = int(run_command(f"sudo oneimage create -d 1 --name {image_name} --type DATABLOCK --size 10 " + " | awk '{print $2}'"))
 
@@ -27,63 +29,78 @@ def prepare_image():
 
 
 @pytest.fixture
-def prepare_file():
-    file_name  = "api_test_file"
+def prepare_image_for_file_ds():
+    image_name = "api_test_file"
     file_path  = "/var/tmp/{file_name}"
     run_command(f"sudo dd if=/dev/urandom of={file_path} bs=1MiB count=10 status=none")
-    file_id    = int(run_command(f"sudo oneimage create -d 2 --name {file_name} --type CONTEXT --path '{file_path}' " + " | awk '{print $2}'"))
+    image_id   = int(run_command(f"sudo oneimage create -d 2 --name {image_name} --type CONTEXT --path '{file_path}' " + " | awk '{print $2}'"))
 
-    yield (file_id, file_name)
+    yield (image_id, image_name)
 
-    run_command(f"sudo oneimage delete {file_id}")
+    run_command(f"sudo oneimage delete {image_id}")
     run_command(f"sudo rm -f {file_path}")
+
 
 
 
 def test_image_not_exist():
     one = One(BRESTADM_SESSION)
-    with pytest.raises(OneNoExistsException, match="Error getting image"):
+    with pytest.raises(OneNoExistsException, match=ERROR_GETTING_IMAGE):
         one.image.chtype(999999, "")
 
 
 
-def test_incompatible_image_type_for_image_datastore(prepare_image):
+def test_incompatible_image_type_for_image_datastore(prepare_image_for_image_ds):
     one                 = One(BRESTADM_SESSION)
-    image_id, _         = prepare_image
+    image_id, _         = prepare_image_for_image_ds
     command_image_type  = f"sudo oneimage show {image_id} | grep TYPE | head -n 1 " + " | awk '{printf $3}'"
     created_image_type  = run_command(command_image_type)
 
     for new_image_type in FILE_TYPES:
         with pytest.raises(OneActionException, match=ERROR_INCOMPATIBLE_TYPE):
             one.image.chtype(image_id, new_image_type)
-        image_type = run_command(command_image_type)
-        assert image_type == created_image_type
+
+        current_image_type = run_command(command_image_type)
+        assert current_image_type == created_image_type
 
 
 
 
-def test_incompatible_file_type_for_file_datastore(prepare_file):
+def test_incompatible_file_type_for_file_datastore(prepare_image_for_file_ds):
     one                 = One(BRESTADM_SESSION)
-    file_id, _          = prepare_file
-    command_file_type   = f"sudo oneimage show {file_id} | grep TYPE | head -n 1 " + " | awk '{printf $3}'"
-    created_file_type   = run_command(command_file_type)
+    image_id, _         = prepare_image_for_file_ds
+    command_image_type  = f"sudo oneimage show {image_id} | grep TYPE | head -n 1 " + " | awk '{printf $3}'"
+    created_image_type  = run_command(command_image_type)
 
-    for new_file_type in IMAGE_TYPES:
+    for new_image_type in IMAGE_TYPES:
         with pytest.raises(OneActionException, match=ERROR_INCOMPATIBLE_TYPE):
-            one.image.chtype(file_id, new_file_type)
-        file_type = run_command(command_file_type)
-        assert file_type == created_file_type
+            one.image.chtype(image_id, new_image_type)
+
+        current_image_type = run_command(command_image_type)
+        assert current_image_type == created_image_type
 
 
 
+def test_available_image_types(prepare_image_for_image_ds):
+    one                 = One(BRESTADM_SESSION)
+    image_id, _         = prepare_image_for_file_ds
+    command_image_type  = f"sudo oneimage show {image_id} | grep TYPE | head -n 1 " + " | awk '{printf $3}'"
+
+    for new_image_type in IMAGE_TYPES:
+        one.image.chtype(image_id, new_image_type)
+        current_image_type = run_command(command_image_type)
+        assert current_image_type == new_image_type
 
 
-def test_available_image_types(prepare_image):
-    pass
 
+def test_available_file_types(prepare_image_for_file_ds):
+    one                 = One(BRESTADM_SESSION)
+    image_id, _         = prepare_image_for_file_ds
+    command_image_type  = f"sudo oneimage show {image_id} | grep TYPE | head -n 1 " + " | awk '{printf $3}'"
 
-
-def test_available_file_types(prepare_file):
-    pass
+    for new_image_type in FILE_TYPES:
+        one.image.chtype(image_id, new_image_type)
+        current_image_type = run_command(command_image_type)
+        assert current_image_type == new_image_type
 
 
