93bd9e89261e71a61cc87cc7669dd0523318e969
Gregory Vetkin <gregoryvetkin@gmail.com>
2024-10-28 15:58:04 +0300

    Refactored one.image.chmod

--- commands/images.py
+++ commands/images.py
@@ -16,3 +16,18 @@ def get_image_group(image_id: int) -> str:
     command = f"sudo oneimage show {image_id} | grep GROUP | head -n 1 " + " | awk '{print $3}'"
     return run_command(command)
 
+
+def get_image_user_rights(image_id: int) -> str:
+    command = f"sudo oneimage show {image_id} | grep -A 4 PERMISSIONS | grep OWNER " + " | awk '{print $3}'"
+    return run_command(command)
+
+
+def get_image_group_rights(image_id: int) -> str:
+    command = f"sudo oneimage show {image_id} | grep -A 4 PERMISSIONS | grep GROUP " + " | awk '{print $3}'"
+    return run_command(command)
+
+
+def get_image_other_rights(image_id: int) -> str:
+    command = f"sudo oneimage show {image_id} | grep -A 4 PERMISSIONS | grep OTHER " + " | awk '{print $3}'"
+    return run_command(command)
+
--- tests/image/test_chmod.py
+++ tests/image/test_chmod.py
@@ -1,90 +1,93 @@
 import pytest
 
-from api    import One
-from pyone  import OneServer, OneActionException, OneNoExistsException, OneException
-from utils  import get_brestadm_auth, run_command
+from api                import One
+from pyone              import OneServer, OneActionException, OneNoExistsException, OneException
+from utils              import get_brestadm_auth, run_command
+from commands.images    import get_image_user_rights, get_image_group_rights, get_image_other_rights
 
 
 URI                 = "http://localhost:2633/RPC2"
 BRESTADM_AUTH       = get_brestadm_auth()
 BRESTADM_SESSION    = OneServer(URI, BRESTADM_AUTH)
 
+ERROR_GETTING_IMAGE = "Error getting image"
+
+
+@pytest.fixture
+def prepare_datablock_with_000_rights():
+    image_name  = "test_change_image_rights"
+    image_id    = int(run_command(f"sudo oneimage create -d 1 --name {image_name} --type DATABLOCK --size 10 " + " | awk '{print $2}'"))
+    run_command(f"sudo oneimage chmod {image_id} 000")
+    yield   image_id, image_name
+    run_command(f"sudo oneimage delete {image_name}")
+
+
 
 def test_image_not_exist():
     one = One(BRESTADM_SESSION)
-    with pytest.raises(OneNoExistsException, match="Error getting image"):
+    with pytest.raises(OneNoExistsException, match=ERROR_GETTING_IMAGE):
         one.image.chmod(999999)
 
 
-def test_change_image_rights():
+def test_change_image_rights(prepare_datablock_with_000_rights):
     one         = One(BRESTADM_SESSION)
-    image_name  = "test_change_image_rights"
-    image_id    = int(run_command(f"sudo oneimage create -d 1 --name {image_name} --type DATABLOCK --size 10 " + " | awk '{print $2}'"))
-
-    command_owner_rights = f"sudo oneimage show {image_id} | grep -A 4 PERMISSIONS | grep OWNER " + " | awk '{print $3}'"
-    command_group_rights = f"sudo oneimage show {image_id} | grep -A 4 PERMISSIONS | grep GROUP " + " | awk '{print $3}'"
-    command_other_rights = f"sudo oneimage show {image_id} | grep -A 4 PERMISSIONS | grep OTHER " + " | awk '{print $3}'"
-
-    run_command(f"sudo oneimage chmod {image_id} 000")
-
-
+    image_id, _ = prepare_datablock_with_000_rights
+    
+    
     one.image.chmod(image_id, user_use=1)
-    assert run_command(command_owner_rights) == "u--"
+    assert get_image_user_rights(image_id) == "u--"
 
     one.image.chmod(image_id, user_manage=1)
-    assert run_command(command_owner_rights) == "um-"
+    assert get_image_user_rights(image_id) == "um-"
 
     one.image.chmod(image_id, user_admin=1)
-    assert run_command(command_owner_rights) == "uma"
+    assert get_image_user_rights(image_id) == "uma"
 
     one.image.chmod(image_id, user_admin=0)
-    assert run_command(command_owner_rights) == "um-"
+    assert get_image_user_rights(image_id) == "um-"
 
     one.image.chmod(image_id, user_manage=0)
-    assert run_command(command_owner_rights) == "u--"
+    assert get_image_user_rights(image_id) == "u--"
     
     one.image.chmod(image_id, user_use=0)
-    assert run_command(command_owner_rights) == "---"
+    assert get_image_user_rights(image_id) == "---"
 
 
 
     one.image.chmod(image_id, group_use=1)
-    assert run_command(command_group_rights) == "u--"
+    assert get_image_group_rights(image_id) == "u--"
 
     one.image.chmod(image_id, group_manage=1)
-    assert run_command(command_group_rights) == "um-"
+    assert get_image_group_rights(image_id) == "um-"
 
     one.image.chmod(image_id, group_admin=1)
-    assert run_command(command_group_rights) == "uma"
+    assert get_image_group_rights(image_id) == "uma"
 
     one.image.chmod(image_id, group_admin=0)
-    assert run_command(command_group_rights) == "um-"
+    assert get_image_group_rights(image_id) == "um-"
 
     one.image.chmod(image_id, group_manage=0)
-    assert run_command(command_group_rights) == "u--"
+    assert get_image_group_rights(image_id) == "u--"
     
     one.image.chmod(image_id, group_use=0)
-    assert run_command(command_group_rights) == "---"
+    assert get_image_group_rights(image_id) == "---"
 
 
 
     one.image.chmod(image_id, other_use=1)
-    assert run_command(command_other_rights) == "u--"
+    assert get_image_other_rights(image_id) == "u--"
 
     one.image.chmod(image_id, other_manage=1)
-    assert run_command(command_other_rights) == "um-"
+    assert get_image_other_rights(image_id) == "um-"
 
     one.image.chmod(image_id, other_admin=1)
-    assert run_command(command_other_rights) == "uma"
+    assert get_image_other_rights(image_id) == "uma"
 
     one.image.chmod(image_id, other_admin=0)
-    assert run_command(command_other_rights) == "um-"
+    assert get_image_other_rights(image_id) == "um-"
 
     one.image.chmod(image_id, other_manage=0)
-    assert run_command(command_other_rights) == "u--"
+    assert get_image_other_rights(image_id) == "u--"
     
     one.image.chmod(image_id, other_use=0)
-    assert run_command(command_other_rights) == "---"
-
-    
-    run_command(f"sudo oneimage delete {image_name}")
+    assert get_image_other_rights(image_id) == "---"
