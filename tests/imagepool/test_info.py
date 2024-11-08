672407a6eb45cce16e53ae51348bff5549c7abd0
gvetkin <gvetkin@astralinux.ru>
2024-11-08 11:05:56 +0300

    Tests one.imagepool refactored

--- tests/imagepool/test_info.py
+++ tests/imagepool/test_info.py
@@ -1,28 +1,39 @@
 import pytest
-
-from api            import One
-from pyone          import OneServer
-from utils          import get_user_auth
-from one_cli.image  import Image, create_image_by_tempalte
-from config         import API_URI, BRESTADM
-
-
-
-BRESTADM_AUTH     = get_user_auth(BRESTADM)
-BRESTADM_SESSION  = OneServer(API_URI, BRESTADM_AUTH)
+from typing             import List
+from api                import One
+from utils              import get_user_auth
+from one_cli.image      import Image, create_image_by_tempalte
+from one_cli.datastore  import Datastore, create_ds_by_tempalte
+from config             import BRESTADM
 
 
+BRESTADM_AUTH = get_user_auth(BRESTADM)
 
-@pytest.fixture
-def prepare_images():
+
+@pytest.fixture(scope="module")
+def datastore():
+    datastore_template = """
+        NAME   = api_test_image_ds
+        TYPE   = IMAGE_DS
+        TM_MAD = ssh
+        DS_MAD = fs
+    """
+    datastore_id = create_ds_by_tempalte(datastore_template)
+    datastore    = Datastore(datastore_id)
+    yield datastore
+    datastore.delete()
+
+
+@pytest.fixture(scope="module")
+def images(datastore: Datastore):
     image_list = []
     for _ in range(5):
         template = f"""
             NAME = api_test_image_{_}
             TYPE = DATABLOCK
-            SIZE = 10
+            SIZE = 1
         """
-        image_id = create_image_by_tempalte(1, template, False)
+        image_id = create_image_by_tempalte(datastore._id, template, False)
         image    = Image(image_id)
         image_list.append(image)
 
@@ -40,40 +51,34 @@ def prepare_images():
 
 
 
-def test_show_all_images(prepare_images):
-    one         = One(BRESTADM_SESSION)
-    image_ids   = [image.info().ID for image in prepare_images]
+@pytest.mark.parametrize("one", [BRESTADM_AUTH], indirect=True)
+def test_show_all_images(one: One, images: List[Image]):
+    image_ids     = [image._id for image in images]
+    imagepool     = one.imagepool.info().IMAGE
+    imagepool_ids = [image.ID for image in imagepool]
 
-    imagepool       = one.imagepool.info().IMAGE
-    imagepool_ids   = [image.ID for image in imagepool]
-    
     assert set(image_ids).issubset(imagepool_ids)
 
 
 
-def test_filter_start_id(prepare_images):
-    one         = One(BRESTADM_SESSION)
-    image_ids   = [image.info().ID for image in prepare_images]
+@pytest.mark.parametrize("one", [BRESTADM_AUTH], indirect=True)
+def test_filter_start_id(one: One, images: List[Image]):
+    image_ids     = [image._id for image in images]
     image_ids.sort()
-
-    imagepool       = one.imagepool.info(start_id=image_ids[1]).IMAGE
-    imagepool_ids   = [image.ID for image in imagepool]
+    imagepool     = one.imagepool.info(start_id=image_ids[1]).IMAGE
+    imagepool_ids = [image.ID for image in imagepool]
     
-
     assert image_ids[0] not in imagepool_ids
     assert set(image_ids[1:]).issubset(imagepool_ids)
 
 
 
-def test_filter_end_id(prepare_images):
-    one         = One(BRESTADM_SESSION)
-    image_ids   = [image.info().ID for image in prepare_images]
+@pytest.mark.parametrize("one", [BRESTADM_AUTH], indirect=True)
+def test_filter_end_id(one: One, images: List[Image]):
+    image_ids     = [image._id for image in images]
     image_ids.sort()
-
-    imagepool       = one.imagepool.info(start_id=image_ids[0], end_id=image_ids[-2]).IMAGE
-    imagepool_ids   = [image.ID for image in imagepool]
+    imagepool     = one.imagepool.info(start_id=image_ids[0], end_id=image_ids[-2]).IMAGE
+    imagepool_ids = [image.ID for image in imagepool]
     
-
     assert image_ids[-1] not in imagepool_ids
     assert set(image_ids[:-2]).issubset(imagepool_ids)
-
