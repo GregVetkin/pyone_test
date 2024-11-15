cce1dd3e2d541237fec099aa55fd4bc5dbd4b68b
gvetkin <gvetkin@astralinux.ru>
2024-11-15 12:24:01 +0300

    Added cluster_test for one.host.allocate

--- tests/host/test_allocate.py
+++ tests/host/test_allocate.py
@@ -3,12 +3,20 @@ import pytest
 from api                import One
 from pyone              import OneNoExistsException, OneInternalException
 from utils              import get_user_auth, get_unic_name
+from one_cli.cluster    import Cluster, create_cluster
 from one_cli.host       import Host, host_exist
 from config             import BRESTADM
 
 
 BRESTADM_AUTH = get_user_auth(BRESTADM)
 
+
+@pytest.fixture
+def cluster():
+    _id = create_cluster(get_unic_name())
+    cluster = Cluster(_id)
+    yield cluster
+    cluster.delete()
 
 
 
@@ -26,12 +34,20 @@ def test_cluster_not_exist(one: One):
 
 
 
+@pytest.mark.parametrize("one", [BRESTADM_AUTH], indirect=True)
+def test_allocate_host_kvm_kvm(one: One):
+    _id  = one.host.allocate(hostname=f"{get_unic_name()}", im_mad="kvm", vm_mad="kvm", cluster_id=-1)
+    assert host_exist(_id)
+    Host(_id).delete()
+
 
 
 @pytest.mark.parametrize("one", [BRESTADM_AUTH], indirect=True)
-def test_allocate_host_kvm_kvm(one: One):
-    _id = one.host.allocate(hostname=f"{get_unic_name()}", im_mad="kvm", vm_mad="kvm", cluster_id=-1)
+def test_create_host_with_certain_cluster(one: One, cluster: Cluster):
+    _id  = one.host.allocate(f"NAME={get_unic_name()}\nTM_MAD=ssh\nDS_MAD=fs", cluster_id=cluster._id)
+    host = Host(_id)
     assert host_exist(_id)
-    Host(_id).delete()
+    assert cluster._id == host.info().CLUSTER_ID
+    host.delete()
 
 
