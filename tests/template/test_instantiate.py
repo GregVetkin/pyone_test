1e11dcc357216691fb5566e6c01919c509942ba4
Gregory Vetkin <gregoryvetkin@gmail.com>
2024-11-19 17:22:37 +0300

    Added some tests for one.template.instantiate

--- tests/template/test_instantiate.py
+++ tests/template/test_instantiate.py
@@ -4,6 +4,7 @@ from api                import One
 from pyone              import OneNoExistsException
 from utils              import get_unic_name
 from one_cli.template   import Template, create_template, template_exist
+from one_cli.vm         import VirtualMachine, create_vm, vm_exist
 from config             import ADMIN_NAME
 
 
@@ -37,5 +38,33 @@ def test_template_not_exist(one: One):
     with pytest.raises(OneNoExistsException):
         one.template.instantiate(999999)
 
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_instantiane_vm(one: One, vmtemplate: Template):
+    vm_id = one.template.instantiate(vmtemplate._id, vm_name=get_unic_name())
+    vm    = VirtualMachine(vm_id)
+    assert vm_exist(vm._id)
+    vm.terminate()
+
+
+
+@pytest.mark.skip(reason="Дописать метод info для vm")
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_instantiane_and_hold_vm(one: One, vmtemplate: Template):
+    vm_id = one.template.instantiate(vmtemplate._id, vm_name=get_unic_name(), hold_vm=True)
+    vm    = VirtualMachine(vm_id)
+    assert vm.info().STATE == 2
+    vm.terminate()
+
+
+
+@pytest.mark.skip(reason="Дописать метод info для vm")
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_instantiane_vm_with_extra_tempalte(one: One, vmtemplate: Template):
+    extra_attribute = "TEST_ATTRIBUTE"
+    vm_id = one.template.instantiate(vmtemplate._id, vm_name=get_unic_name(), extra_template=f"{extra_attribute} = TEST")
+    vm    = VirtualMachine(vm_id)
+    assert extra_attribute in vm.info().TEMPLATE
+    vm.terminate()
 
 
