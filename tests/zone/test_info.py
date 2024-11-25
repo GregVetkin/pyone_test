81855572c566d61611f49d65fc01e595ae752d43
Gregory Vetkin <gregoryvetkin@gmail.com>
2024-11-25 17:29:55 +0300

    Created tests for one.zone.info method

--- tests/zone/test_info.py
+++ tests/zone/test_info.py
@@ -1,10 +1,28 @@
+import pytest
+
+from api                import One
+from utils              import get_unic_name
+from one_cli.zone       import Zone, create_zone, zone_exist
+from config             import ADMIN_NAME
+
+from tests._common_tests.info import info_if_not_exist__test
+from tests._common_tests.info import info__test
 
 
 
 
-
-
-
+@pytest.fixture
+def zone():
+    template = f"""
+        NAME     = {get_unic_name()}
+        ENDPOINT = http://localhost:2633/RPC2
+    """
+    _id = create_zone(template)
+    zone = Zone(_id)
+    yield zone
+    if not zone_exist(_id):
+        return
+    zone.delete()
 
 
 
@@ -13,11 +31,14 @@
 # =================================================================================================
 
 
-def test_zone_not_exist():
-    assert False, "Тест в разработке"
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_zone_not_exist(one: One):
+    info_if_not_exist__test(one.zone)
 
 
 
-def test_zone_info():
-    assert False, "Тест в разработке"
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_zone_info(one: One, zone: Zone):
+    info__test(one.zone, zone)
 
