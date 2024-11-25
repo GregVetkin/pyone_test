f973c736be713de75ccef5b9c5d6db1319ccd96c
Gregory Vetkin <gregoryvetkin@gmail.com>
2024-11-25 18:09:12 +0300

    Created tests for one.zonepool.info

--- tests/zone/test_info.py
+++ tests/zone/test_info.py
@@ -2,7 +2,7 @@ import pytest
 
 from api                import One
 from utils              import get_unic_name
-from one_cli.zone       import Zone, create_zone, zone_exist
+from one_cli.zone       import Zone, create_zone
 from config             import ADMIN_NAME, API_URI
 
 from tests._common_tests.info import info_if_not_exist__test
--- tests/zonepool/test_info.py
+++ tests/zonepool/test_info.py
@@ -1,15 +1,32 @@
+import pytest
+
+from api                import One
+from utils              import get_unic_name
+from one_cli.zone       import Zone, create_zone
+from config             import ADMIN_NAME, API_URI
+from typing             import List
 
 
 
 
 
 
+@pytest.fixture
+def zones():
+    zone_list = []
+    for _ in range(5):
+        template = f"""
+            NAME     = {get_unic_name()}
+            ENDPOINT = {API_URI}
+        """
+        zone_id = create_zone(template)
+        zone    = Zone(zone_id)
+        zone_list.append(zone)
 
+    yield zone_list
 
-
-
-
-
+    for zone in zone_list:
+        zone.delete()
 
 
 
@@ -20,6 +37,11 @@
 
 
 
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_show_all_zones(one: One, zones: List[Zone]):
+    zone_ids       = [zone.info().ID for zone in zones]
+    zonepool       = one.zonepool.info().ZONE
+    zonepool_ids   = [zone.ID for zone in zonepool]
+    
+    assert set(zone_ids).issubset(zonepool_ids)
 
-def test_show_all_zones():
-    assert False, "Тест в разработке"
