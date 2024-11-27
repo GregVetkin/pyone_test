4aecdbb0778a4279556b4f0f2f1ff14bb3649342
gvetkin <gvetkin@astralinux.ru>
2024-11-27 15:50:28 +0300

    Added tests for one.cluster.adddatastore method

---
+++ tests/cluster/test_adddatastore.py
@@ -0,0 +1,91 @@
+import pytest
+
+from api                import One
+from pyone              import OneException
+from utils              import get_unic_name
+from one_cli.cluster    import Cluster, create_cluster, cluster_exist
+from one_cli.datastore  import Datastore, create_datastore
+from config             import ADMIN_NAME
+
+
+
+
+@pytest.fixture
+def datastore():
+    datastore_template = f"""
+        NAME   = {get_unic_name()}
+        TYPE   = SYSTEM_DS
+        TM_MAD = ssh
+    """
+    datastore_id = create_datastore(datastore_template)
+    datastore    = Datastore(datastore_id)
+    yield datastore
+    datastore.delete()
+
+
+
+@pytest.fixture
+def cluster():
+    cluster_id = create_cluster(get_unic_name())
+    cluster    = Cluster(cluster_id)
+    yield cluster
+    cluster.delete()
+
+
+
+@pytest.fixture
+def cluster_with_datastore(datastore):
+    cluster_id = create_cluster(get_unic_name())
+    cluster    = Cluster(cluster_id)
+    cluster.adddatastore(datastore._id)
+    yield cluster
+
+    if cluster_exist(cluster_id):
+        cluster.deldatastore(datastore._id)
+        cluster.delete()
+
+
+
+# =================================================================================================
+# TESTS
+# =================================================================================================
+
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_cluster_not_exist(one: One, datastore: Datastore):
+    with pytest.raises(OneException):
+        one.cluster.adddatastore(999999, datastore._id)
+   
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_datastore_not_exist(one: One, cluster: Cluster):
+    with pytest.raises(OneException):
+        one.cluster.adddatastore(cluster._id, 999999)
+   
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_add_datastore_to_cluster(one: One, cluster: Cluster, datastore: Datastore):
+    assert datastore._id not in cluster.info().DATASTORES
+    one.cluster.adddatastore(cluster._id, datastore._id)
+    assert datastore._id in cluster.info().DATASTORES
+
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_add_datastore_to_cluster(one: One, cluster: Cluster, datastore: Datastore):
+    assert datastore._id not in cluster.info().DATASTORES
+    one.cluster.adddatastore(cluster._id, datastore._id)
+    assert datastore._id in cluster.info().DATASTORES
+
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_add_added_datastore_to_cluster(one: One, cluster_with_datastore: Cluster):
+    old_cluster_datastores = cluster_with_datastore.info().DATASTORES
+    assert old_cluster_datastores
+    added_datastore_id = old_cluster_datastores[0]
+
+    one.cluster.adddatastore(cluster_with_datastore._id, added_datastore_id)
+
+    new_cluster_datastores = cluster_with_datastore.info().DATASTORES
+    assert added_datastore_id in new_cluster_datastores
+    assert len(old_cluster_datastores) == len(new_cluster_datastores)
+
