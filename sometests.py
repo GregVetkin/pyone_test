d527dae559b4e19e62f27bd284296b21664d3959
gvetkin <gvetkin@astralinux.ru>
2025-02-12 13:30:07 +0300

    Added neew tests for one.vm.deploy method

--- sometests.py
+++ sometests.py
@@ -5,7 +5,7 @@ from api            import One
 from config         import API_URI, ADMIN_NAME
 
 
-brestadm_auth = ADMIN_NAME+":"+"fa20ef55f090e3e00980a25b2f3876725b4aa6c6d32239410c802e5912487715"
+brestadm_auth = ADMIN_NAME+":"+"92e18650e8b0f8cd173e01115798937e9c632315693982f526b8be10adee29bc"
 
 API_URI = "http://bufn1.brest.local:2633/RPC2"
 
@@ -14,15 +14,5 @@ one = One(pyone.OneServer(API_URI, brestadm_auth))
 
 
 
-records = one.vmpool.monitoring().MONITORING
-print(len(records))
 
-
-counter = 0
-
-while True:
-    records = one.vmpool.monitoring().MONITORING
-    time.sleep(1)
-    counter += 1
-    print("Second ", counter, " | ", "Records: ", len(records))
-
+print(type(one.vm.info(0).HISTORY_RECORDS.HISTORY[-1].DS_ID))
--- tests/vm/test_deploy.py
+++ tests/vm/test_deploy.py
@@ -5,31 +5,49 @@ from time            import sleep
 from api             import One
 from utils           import get_unic_name
 from one_cli.vm      import VirtualMachine
-from config          import ADMIN_NAME
+from config          import ADMIN_NAME, VmStates
 
 
-
-def await_vm_status_code(one: One, vm_id: int, status_code: int, intervals=1.0):
-    while one.vm.info(vm_id).STATE != status_code:
-        sleep(intervals)
 
 
 @pytest.fixture()
 @pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
 def undeployed_vm(one: One): 
     vm_id = one.vm.allocate(f"NAME={get_unic_name()}\nCPU=0.1\nMEMORY=1\n")
-    await_vm_status_code(one, vm_id, 8)
+    while one.vm.info(vm_id).STATE != VmStates.POWEROFF: sleep(0.5)
+
     one.vm.action("undeploy-hard", vm_id)
-    sleep(2)
-    await_vm_status_code(one, vm_id, 9)
+    while one.vm.info(vm_id).STATE != VmStates.UNDEPLOYED: sleep(0.5)
 
     yield VirtualMachine(vm_id)
 
     one.vm.action("terminate-hard", vm_id)
+    while one.vm.info(vm_id).STATE != VmStates.DONE: sleep(0.5)
 
 
 
+@pytest.fixture()
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def large_hold_vm(one: One):
+    vm_id = one.vm.allocate(f"NAME={get_unic_name()}\nCPU=99999\nMEMORY=1\n", True)
+    while one.vm.info(vm_id).STATE != VmStates.HOLD: sleep(0.5)
 
+    yield VirtualMachine(vm_id)
+
+    one.vm.recover(vm_id, 3)
+    while one.vm.info(vm_id).STATE != VmStates.DONE: sleep(0.5)
+
+
+@pytest.fixture()
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def hold_vm(one: One):
+    vm_id = one.vm.allocate(f"NAME={get_unic_name()}\nCPU=1\nMEMORY=1\n", True)
+    while one.vm.info(vm_id).STATE != VmStates.HOLD: sleep(0.5)
+
+    yield VirtualMachine(vm_id)
+
+    one.vm.recover(vm_id, 3)
+    while one.vm.info(vm_id).STATE != VmStates.DONE: sleep(0.5)
 
 
 # =================================================================================================
@@ -44,21 +62,50 @@ def test_vm_not_exist(one: One):
 
 
 @pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
-def test_host_not_exist(one: One, undeployed_vm: VirtualMachine):
+def test_host_not_exist(one: One, hold_vm: VirtualMachine):
     with pytest.raises(OneException):
-        one.vm.deploy(undeployed_vm._id, 999999)
-
+        one.vm.deploy(hold_vm._id, 999999)
 
 
 @pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
 def test_deploy_on_host(one: One, undeployed_vm: VirtualMachine):
+    vm_id           = undeployed_vm._id
     host_ids_list   = [host.ID for host in one.hostpool.info().HOST]
     target_host_id  = random.choice(host_ids_list)
 
-    _id = one.vm.deploy(undeployed_vm._id, target_host_id)
-    assert _id == undeployed_vm._id
+    _id = one.vm.deploy(vm_id, target_host_id)
+    assert _id == vm_id
+    while one.vm.info(vm_id).STATE != VmStates.POWEROFF: sleep(0.5)
+    assert vm_id in one.host.info(target_host_id).VMS.ID
 
-    sleep(5)
-    
-    assert undeployed_vm._id in one.host.info(target_host_id).VMS.ID
+
+@pytest.mark.parametrize("check_capacity", [True, False])
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_check_host_capacity(one: One, large_hold_vm: VirtualMachine, check_capacity: bool):
+    vm_id           = large_hold_vm._id
+    host_ids_list   = [host.ID for host in one.hostpool.info().HOST]
+    target_host_id  = random.choice(host_ids_list)
+
+    if check_capacity:
+        with pytest.raises(OneException):
+            one.vm.deploy(vm_id, target_host_id, check_capacity)
+    else:
+        _id = one.vm.deploy(vm_id, target_host_id, check_capacity)
+        assert _id == vm_id 
+
+
+
+@pytest.mark.parametrize("one", [ADMIN_NAME], indirect=True)
+def test_certain_datastore(one: One, hold_vm: VirtualMachine):
+    vm_id           = hold_vm._id
+    host_ids        = [host.ID for host in one.hostpool.info().HOST]
+    system_ds_ids   = [datastore.ID for datastore in one.datastorepool.info().DATASTORE if datastore.TYPE == 1]
+    target_host_id  = random.choice(host_ids)
+    target_ds_id    = random.choice(system_ds_ids)
+
+    _id = one.vm.deploy(vm_id, target_host_id, datastore_id=target_ds_id)
+    assert _id == vm_id
+    # sleep(5)
+    assert one.vm.info(vm_id).HISTORY_RECORDS.HISTORY[-1].DS_ID == target_ds_id
+
 
