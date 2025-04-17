5db5334dcce56dceece1fd38f4e4cd82b6dd9c80
gvetkin <gvetkin@astralinux.ru>
2025-04-17 14:45:34 +0300

    Back on track

--- sometests.py
+++ sometests.py
@@ -1,20 +1,28 @@
+
 import pyone
-import time
-
 from api            import One
-from config         import API_URI, ADMIN_NAME, VmStates, VmLcmStates
+from config         import API_URI, ADMIN_NAME
 
 
-brestadm_auth = ADMIN_NAME+":"+"92e18650e8b0f8cd173e01115798937e9c632315693982f526b8be10adee29bc"
+brestadm_auth = ADMIN_NAME+":"+"0776469fdb010e361440f8eacde8a872ddf36a6b9f8053ff84016e2621a7c797"
+# brestadm_auth = "tester:f93c22104ac19e027d6259153bc1cba6e98c93337d87ef695cc95ea6f18ad617"
 
-API_URI = "http://bufn1.brest.local:2633/RPC2"
-
-
-
+API_URI = "http://raft.brest.local:2633/RPC2"
 
 
 one = One(pyone.OneServer(API_URI, brestadm_auth))
 
+templ = """
+CPU=99999
+"""
 
-while True:
-    print(one.vm.info(48).STATE)
+res = one.vm.resize(36, templ, False)
+print(res)
+
+
+
+# import pyone
+
+# client = pyone.OneServer(API_URI, session=brestadm_auth)
+# res = client.vm.attachsg()
+# print(res)
