024b8bf28d5810aba74a8fa4f29090f152104dae
gvetkin <gvetkin@astralinux.ru>
2025-05-14 16:30:49 +0300

    Corrected tests for one.image.restore for Brest 4

--- send_on_bufn1.sh
+++ send_on_bufn1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
+
 ssh u@bufn1 "sudo rm -r ~/brest/api/pyone/"
-scp -r ~/Документы/Projects/pyone_test u@bufn1:/home/u/brest/api/
+scp -r $SCRIPT_DIR u@bufn1:/home/u/brest/api/
 ssh u@bufn1 "sudo chmod 777 -R ~/brest/api/pyone_test" 
 ssh u@bufn1 "sudo rm -rf ~/brest/api/pyone_test/.venv"
 ssh u@bufn1 "sudo mv ~/brest/api/pyone_test ~/brest/api/pyone"
-# ssh u@bufn1 "~/brest/pyone_test/prepare.sh"
