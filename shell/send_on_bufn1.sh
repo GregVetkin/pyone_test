ed065a555d91794551ac65889c6e60733b2e61a4
gvetkin <gvetkin@astralinux.ru>
2025-05-15 17:33:09 +0300

    Changed send script

--- shell/send_on_bufn1.sh
+++ shell/send_on_bufn1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ssh u@bufn1 "sudo rm -r ~/brest/pyone_refactor/"
+SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE[0]}"))
+PARENT_SCRIPT_DIR=$(dirname $SCRIPT_DIR)
 
-scp -r ~/Документы/pyone_refactor/ u@bufn1:/home/u/brest/
-
-ssh u@bufn1 "sudo chmod 777 -R ~/brest/pyone_refactor/" 
-
-ssh u@bufn1 "sudo rm -rf ~/brest/pyone_refactor/.venv"
+ssh u@bufn1 "sudo rm -r ~/brest/api/pyone/"
+scp -r $PARENT_SCRIPT_DIR u@bufn1:/home/u/brest/api/
+ssh u@bufn1 "sudo chmod 777 -R ~/brest/api/pyone_test" 
+ssh u@bufn1 "sudo rm -rf ~/brest/api/pyone_test/.venv"
+ssh u@bufn1 "sudo mv ~/brest/api/pyone_test ~/brest/api/pyone"
