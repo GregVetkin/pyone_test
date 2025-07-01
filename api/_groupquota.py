95d28e0df03454989975151815c5db705d33a58f
gvetkin <gvetkin@astralinux.ru>
2025-07-01 16:02:48 +0300

    Minor changes

--- api/_groupquota.py
+++ api/_groupquota.py
@@ -1,16 +1,16 @@
 from api.one        import OneServer
-
+from pyone.bindings import DEFAULT_GROUP_QUOTASTypeSub
 
 
 class OneGroupquota:
     def __init__(self, one_api: OneServer) -> None:
         self._one_groupquota = one_api.groupquota
 
-    def info(self) -> str:
+    def info(self) -> DEFAULT_GROUP_QUOTASTypeSub:
         """Returns the default group quota limits"""
         return self._one_groupquota.info()
     
-    def update(self, template: str) -> str:
+    def update(self, template: str) -> DEFAULT_GROUP_QUOTASTypeSub:
         """Updates the default group quota limits"""
         return self._one_groupquota.update(template)
     
