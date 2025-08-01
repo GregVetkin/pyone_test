fdb0823bd4b8d70b81a88af7bceeb7136c743bcb
gvetkin <gvetkin@astralinux.ru>
2025-08-01 10:58:04 +0300

    Refactored: one.vmpool.info

--- z.py
+++ z.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
 
     one = pyone.OneServer(endpoint, session=f"{username}:7b7d2f5dbd20988ce938deaf04a7979899f35a67a47dbb7461ba351edb7a9305")
     # template = f'<USER_TEMPLATE><SCHED_DS_REQUIREMENTS><![CDATA[ID="99999"]]></SCHED_DS_REQUIREMENTS></USER_TEMPLATE>'
-    print(0%2)
+    print(one.vmpool.info(-2, -1, -1, -1, "").VM)
     # print(one.vm.info(0).SNAPSHOTS[0].SNAPSHOT[-1].CHILDREN)
 
 
